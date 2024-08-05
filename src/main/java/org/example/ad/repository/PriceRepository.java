58c6d3c9a62a394fb791e6c4878b1df9e23d1291
YuuKiSSa <e1241986@u.nus.edu>
2024-08-05 13:14:57 +0800

    add a constraint on username

--- src/main/java/org/example/ad/repository/PriceRepository.java
+++ src/main/java/org/example/ad/repository/PriceRepository.java
@@ -13,6 +13,6 @@ public interface PriceRepository extends JpaRepository<Price, Long> {
 
 	List<Price> findByCameraIdAndPlatform(Long cameraId, Platform platform);
 
-	@Query("SELECT MIN(p.price) FROM Price p WHERE p.camera.id = :cameraId AND p.date = (SELECT MAX(p.date) FROM Price p2 WHERE p2.camera.id = :cameraId)")
+	@Query("SELECT MIN(p.price) FROM Price p WHERE p.camera.id = :cameraId AND p.date = (SELECT MAX(p2.date) FROM Price p2 WHERE p2.camera.id = :cameraId)")
 	Double findLatestLowestPriceByCameraId(@Param("cameraId") Long cameraId);
 }
