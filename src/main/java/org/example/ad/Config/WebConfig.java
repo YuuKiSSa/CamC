90e55403f3c52da5f58e106c06abebeb2f3b0cca
YuuKiSSa <e1241986@u.nus.edu>
2024-08-13 23:19:14 +0800

    modify WebConfig

--- src/main/java/org/example/ad/Config/WebConfig.java
+++ src/main/java/org/example/ad/Config/WebConfig.java
@@ -11,7 +11,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:3000")
+                .allowedOrigins("http://localhost:3000", "http://192.168.0.201:3000", "http://10.0.2.2:3000", "http://13.213.1.218")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
